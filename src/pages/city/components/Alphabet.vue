dd656e1b2f8a5cd449a15fc41088c04f8959d6f5
loveapple-pan <1104483764@qq.com>
2018-08-05 22:05:36 +0800

    城市列表页面完成

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+        <div class="item">A</div>
+        <div class="item">A</div>
+        <div class="item">A</div>
+        <div class="item">A</div>
+        <div class="item">A</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    right 0
+    top 1.58rem
+    bottom 0
+    width .4rem
+    .item
+        text-align center
+        line-height .4rem
+        color $bgColor
+
+</style>
