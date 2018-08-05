244a4a8e0e0db3765fa9aed0b4377a190fa8a444
loveapple-pan <1104483764@qq.com>
2018-08-05 22:21:25 +0800

    合并

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -8,11 +8,32 @@
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
     CityHeader,
     CitySearch
+  },
+  data() {
+    return {
+      hotCities: [],
+      cities: {}
+    }
+  },
+  mounted() {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json').then(this.hadInfo)
+    },
+    hadInfo(data) {
+      if (data.ret && data.data) {
+        this.hotCities = data.data.hotCities
+        this.cities = data.data.cities
+      }
+    }
   }
 }
 </script>
